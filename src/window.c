e239d1f8705047e6b4e38d03eb4064addf662112
AmandaWasTaken <amandasandell02@gmail.com>
2025-08-15 00:05:27 -0100

    im a DOUBLE idiot

--- src/window.c
+++ src/window.c
@@ -70,7 +70,7 @@ void init_window(const char* restrict content, const int time){
 		die("Failed to initialize SDL");
 	}
 		
-//	usleep(time * 60 * 1000000ULL);
+	usleep(time * 60 * 1000000ULL);
 	play_async();
 
 	// @param title(str)
